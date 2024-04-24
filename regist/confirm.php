05049d85f6c5388b68ce8b01f8e608dbe985f9d3
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-04-24 10:05:56 +0900

    update

--- regist/confirm.php
+++ regist/confirm.php
@@ -1,16 +1,34 @@
-<?php 
+<?php
+// POSTデータ取得
 $posts = $_POST;
-var_dump($posts);
+// var_dump($posts);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h2>Register</h2>
+    <p>この内容で登録しますか？</p>
+    <form action="" method="post">
+        <div>
+            <label for="">Name</label>
+            <p><?= $posts['name'] ?></p>
+        </div>
+        <div>
+            <label for="">Email</label>
+            <p><?= $posts['email'] ?></p>
+        </div>
+        <div>
+            <button>Regist</button>
+        </div>
+    </form>
 </body>
+
 </html>
