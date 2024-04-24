a3a7ebd6cc595028b0a03c0bd15cca050ab10dee
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-04-24 15:05:57 +0900

    update

--- regist/add.php
+++ regist/add.php
@@ -1,8 +1,22 @@
 <?php 
-// TODO: データベースに接続
-// TODO: セッションデータを取得
-// TODO: usersテーブルにレコード追加するSQLを作成
-// TODO: SQLを実行
+// データベースに接続
+require_once '../db.php';
+
+// セッションを開始
+session_start();
+session_regenerate_id(true);
+
+// セッションデータを取得
+if (!empty($_SESSION['my_shop']['regist'])) {
+    $regist = $_SESSION['my_shop']['regist'];
+}
+
+// usersテーブルにレコード追加するSQLを作成
+$sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
+
+// SQLを実行
+$stmt = $pdo->prepare($sql);
+$stmt->execute($regist);
 
 // 完了画面にリダイレクトする
 header('Location: complete.php');
