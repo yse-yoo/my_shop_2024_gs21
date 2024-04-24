8a5cf2312c722da4b777d66196ffe68beb8d85c2
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-04-24 15:29:39 +0900

    update

--- regist/add.php
+++ regist/add.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // データベースに接続
 require_once '../db.php';
 
@@ -9,11 +9,9 @@ session_regenerate_id(true);
 // セッションデータを取得
 if (!empty($_SESSION['my_shop']['regist'])) {
     $regist = $_SESSION['my_shop']['regist'];
+    // パスワードのハッシュ化
+    $regist['password'] = password_hash($regist['password'], PASSWORD_DEFAULT);
 }
-
-// パスワードのハッシュ化
-$regist['password'] = password_hash($regist['password'], PASSWORD_DEFAULT);
-
 // usersテーブルにレコード追加するSQLを作成
 $sql = "INSERT INTO users (name, email, password) VALUES (:name, :email, :password)";
 
@@ -23,4 +21,3 @@ $stmt->execute($regist);
 
 // 完了画面にリダイレクトする
 header('Location: complete.php');
-?>
