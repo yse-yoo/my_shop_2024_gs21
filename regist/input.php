4b9d7a1cf39e99c9de364c6971c9bf68f64e9a71
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-04-24 09:55:11 +0900

    update

--- regist/input.php
+++ regist/input.php
@@ -12,6 +12,14 @@
             <label for="">Name</label>
             <input type="text" name="name">
         </div>
+        <div>
+            <label for="">Email</label>
+            <input type="text" name="email">
+        </div>
+        <div>
+            <label for="">Password</label>
+            <input type="password" name="password">
+        </div>
     </form>
 </body>
 </html>
