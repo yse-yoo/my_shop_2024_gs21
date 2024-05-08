28f8800b7f46cca759549361767d7ef07174d78e
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-05-08 14:12:36 +0900

    duplicaete

--- admin_item/add.php
+++ admin_item/add.php
@@ -2,11 +2,26 @@
 // データベースに接続
 require_once '../db.php';
 
+// POSTデータ取得
+$item = $_POST;
+
+// TODO: 商品コード重複チェック
+// TODO: 商品コードで検索
+$sql = "SELECT * FROM items WHERE code = :code";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['code' => $item['code']]);
+$duplicate_item = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// 商品コードが重複していたら入力画面にリダイレクト
+if ($duplicate_item) {
+   header('Location: input.php');
+   exit;
+}
+
+
 // itemsテーブルにレコード追加するSQLを作成
 $sql = "INSERT INTO items (code, name, price, stock) 
         VALUES (:code, :name, :price, :stock)";
-
-$item = $_POST;
 
 // SQLを実行
 $stmt = $pdo->prepare($sql);
