e55eb73029245cfd9a7b73a60f8415f7168bd6c5
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-05-08 14:05:25 +0900

    update

--- admin_item/delete.php
+++ admin_item/delete.php
@@ -1,7 +1,23 @@
-<?php 
-// TODO: データベースに接続
-// TODO: 商品IDを取得
-// TODO: 商品IDでitemsのレコードを削除するSQL
-// TODO: SQLを実行
-// TODO: 一覧画面にリダイレクトする
-?>
+<?php
+// CRUD
+// Create: INSERT INTO ...
+// Read: SELECT xx FROM ...
+// Update: UPDATE xxx SET xxx = xxx... WHERE ...
+// Delete: DELETE FROM xxxx WHERE xxxx
+
+// データベースに接続
+require_once '../db.php';
+
+// 商品IDを取得
+if (isset($_POST['id'])) {
+    $id = $_POST['id'];
+    // 商品IDでitemsのレコードを削除するSQL
+    $sql = "DELETE FROM items WHERE id = {$id};";
+
+    // SQLを実行
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($item);
+
+    // 一覧画面にリダイレクトする
+    header('Location: ./');
+}
