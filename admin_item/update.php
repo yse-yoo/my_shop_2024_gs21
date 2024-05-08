72c91d20b87055858b710e6ecfc60f4acaab4dd8
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-05-08 13:06:36 +0900

    update

--- admin_item/update.php
+++ admin_item/update.php
@@ -3,13 +3,9 @@
 require_once '../db.php';
 
 // POSTデータ取得
-
 $item = $_POST;
 
-// TODO: 商品 IDを取得
-$id = $item['id'];
-
-// TODO: 指定した商品IDで、データ更新するSQL
+// 指定した商品IDで、データ更新するSQL
 $sql = "UPDATE items SET 
             code = :code,
             name = :name,
@@ -18,9 +14,9 @@ $sql = "UPDATE items SET
         WHERE id = :id
         ";
 
-// TODO: SQLを実行
+// SQLを実行
 $stmt = $pdo->prepare($sql);
 $stmt->execute($item);
 
-// TODO: 一覧画面に戻る（リダイレクト）
+// 一覧画面に戻る（リダイレクト）
 header('Location: ./');
