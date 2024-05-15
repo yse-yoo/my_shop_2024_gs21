6b772b51b056ea866d74255de48745d8525df27c
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-05-15 10:34:19 +0900

    update

---
+++ item/cart.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+
+<body>
+    <main class="container">
+        <h2 class="p-2 text-center">ショッピングカート</h2>
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title">商品1</h5>
+                        <p class="card-text text-danger">&yen;500</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
--- item/index.php
+++ item/index.php
@@ -36,7 +36,7 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                             <div class="card-body">
                                 <h5 class="card-title"><?= $item['name'] ?></h5>
                                 <p class="card-text text-danger">&yen;<?= $item['price'] ?></p>
-                                <a href="#" class="btn btn-primary">カートに入れる</a>
+                                <a href="cart.php?item_id=<?= $item['id'] ?>" class="btn btn-primary">カートに入れる</a>
                             </div>
                         </div>
                     </div>
