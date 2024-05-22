576273f531b2c1646797bc9b8aa423e6a5bf317a
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-05-22 10:50:37 +0900

    update

--- item/cart.php
+++ item/cart.php
@@ -29,6 +29,8 @@ if (isset($_SESSION['my_shop']['cart'])) {
     $cart_items = $_SESSION['my_shop']['cart'];
 }
 // var_dump($_SESSION['my_shop']['cart']);
+
+$amount_list = range(1, 10);
 ?>
 
 <!DOCTYPE html>
@@ -40,7 +42,6 @@ if (isset($_SESSION['my_shop']['cart'])) {
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-
 
 <body>
     <main class="container">
@@ -55,8 +56,18 @@ if (isset($_SESSION['my_shop']['cart'])) {
                             <div class="card-body">
                                 <h5 class="card-title"><?= $cart_item['name'] ?></h5>
                                 <p class="card-text text-danger">&yen;<?= $cart_item['price'] ?></p>
-                                <div>
+                                <div class="d-flex">
                                     <a href="delete.php?item_id=<?= $cart_item['id'] ?>">削除</a>
+                                    <div class="ms-3">
+                                        <form action="" method="get">
+                                            <select name="amount">
+                                                <option value="">-- 個数 --</option>
+                                                <?php foreach ($amount_list as $amount):  ?>
+                                                <option value="<?= $amount ?>"><?= $amount ?></option>
+                                                <?php endforeach  ?>
+                                            </select>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
